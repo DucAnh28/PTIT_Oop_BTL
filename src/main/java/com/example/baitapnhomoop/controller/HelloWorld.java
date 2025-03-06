e75cbac56cfb08413675a0e4c7ba6c7267dc5b10
anhnd <ducanhnguyeny2k@gmail.com>
2025-03-06 09:22:14 +0700

    modify package

--- src/main/java/com/example/baitapnhomoop/controller/HelloWorld.java
+++ src/main/java/com/example/baitapnhomoop/controller/HelloWorld.java
@@ -1,4 +1,4 @@
-package com.example.baitapnhomoop;
+package com.example.baitapnhomoop.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
