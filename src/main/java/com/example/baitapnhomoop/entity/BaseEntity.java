7848a2603106b3423850881583f4c1dec987cb93
anhnd <ducanhnguyeny2k@gmail.com>
2025-03-18 10:02:01 +0700

    ok

--- src/main/java/com/example/baitapnhomoop/entity/BaseEntity.java
+++ src/main/java/com/example/baitapnhomoop/entity/BaseEntity.java
@@ -1,23 +1,47 @@
 package com.example.baitapnhomoop.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
-public class BaseEntity {
+@MappedSuperclass
+@Builder
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    Long id;
 
     @CreatedDate
     @Column(name = "created_date")
-    private LocalDateTime createdDate;
+    LocalDateTime createdDate;
 
     public BaseEntity() {
+    }
+
+    public BaseEntity(LocalDateTime createdDate, Long id) {
+        this.createdDate = createdDate;
+        this.id = id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
     }
 }
--- src/main/java/com/example/baitapnhomoop/repository/CommonRepository.java
+++ src/main/java/com/example/baitapnhomoop/repository/CommonRepository.java
@@ -1,4 +1,15 @@
 package com.example.baitapnhomoop.repository;
 
-public interface CommonRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.io.Serializable;
+
+@NoRepositoryBean
+public interface CommonRepository<T, ID extends Serializable> extends JpaRepository<T, ID>,
+        PagingAndSortingRepository<T, ID>,
+        JpaSpecificationExecutor<T> {
+
 }
