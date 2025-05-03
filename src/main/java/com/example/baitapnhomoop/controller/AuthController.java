ff1502cdbe86d4987b45d2d6dfa4c7c173268e37
anhnd <ducanhnguyeny2k@gmail.com>
2025-05-03 21:06:43 +0700

    update cors config

--- src/main/java/com/example/baitapnhomoop/controller/AuthController.java
+++ src/main/java/com/example/baitapnhomoop/controller/AuthController.java
@@ -7,6 +7,7 @@ import com.example.baitapnhomoop.bean.dto.response.SignupResp;
 import com.example.baitapnhomoop.service.AuthService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin("*")
 public class AuthController {
 
     private final AuthService authService;
--- src/main/java/com/example/baitapnhomoop/controller/admin/AdminController.java
+++ src/main/java/com/example/baitapnhomoop/controller/admin/AdminController.java
@@ -5,6 +5,7 @@ import com.example.baitapnhomoop.bean.dto.CommonResp;
 import com.example.baitapnhomoop.bean.dto.request.BookCreateReq;
 import com.example.baitapnhomoop.service.LibraryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/admin")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class AdminController {
 
     private final LibraryService libraryService;
