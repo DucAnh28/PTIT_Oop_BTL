c8681130a9013f72ddbe364de140fce0828e3ba4
DucAnh <ducanhnguyeny2k@gmail.com>
2025-05-06 19:31:15 +0700

    testing fix CORS

--- src/main/java/com/example/baitapnhomoop/controller/LibraryController.java
+++ src/main/java/com/example/baitapnhomoop/controller/LibraryController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/book")
-@CrossOrigin("*")
+//@CrossOrigin("*")
 public class LibraryController {
 
     private final LibraryService libraryService;
