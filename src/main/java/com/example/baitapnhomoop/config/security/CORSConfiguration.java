3fd3d465047a55b9dfdca09ca0fa65a040b468fb
DucAnh <ducanhnguyeny2k@gmail.com>
2025-05-06 18:59:27 +0700

    testing fix CORS

--- src/main/java/com/example/baitapnhomoop/config/security/CORSConfiguration.java
+++ src/main/java/com/example/baitapnhomoop/config/security/CORSConfiguration.java
@@ -17,8 +17,7 @@ public class CORSConfiguration {
                         .allowedHeaders("*")
                         .allowedMethods("*")
                         .allowedOrigins("*")
-                        .exposedHeaders("Content-Disposition", "Authorization")
-                        .allowCredentials(true);
+                        .exposedHeaders("Content-Disposition", "Authorization", "Content-Type");
             }
         };
     }
